035a71aca2af086aefcc6e6809ea15ee9fd1805b
Erick Ramirez <77713259+Tufowin@users.noreply.github.com>
2021-06-16 12:38:53 -0400

    ⭐ Perfect!

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,19 @@ const exportData = () => {
 getJSON_({
     url: "https://randomuser.me/api/",
     event: data => {
-	// Variables globales!
+		// Variables globales!
     	let name = data.results["0"].name.first;
         let lastname = data.results["0"].name.last;
         let completename = name + " " + lastname;
         let photo = data.results["0"].picture.large;
-    	// Vaciando Contenedor!
+    	 // Crear Tarjeta!
         s_('#root').innerHTML = "";
-	// Crear Tarjeta!
         card_({
             container: "#root",
             content: `
-		<img id="photo src="${photo}" alt="Photo" />
-		<span id="title"> Name: </span>
-		<span id="name"> ${completename} </span>`
+				<img id="photo" src="${photo}" alt="Photo">					
+				<div id="title"> Name: </div>
+				<span id="name"> ${completename} </span> `
         })
     }
 });
